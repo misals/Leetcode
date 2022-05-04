c832ce8ba4cc04bd0953dd16f73d1f96fc4e0497
Suraj Misal <35091245+misals@users.noreply.github.com>
2022-05-04 17:09:34 +0530

    Time: 1180 ms (78.40%), Space: 294.4 MB (38.00%) - LeetHub

---
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        
+        unordered_map<int, TreeNode*> mp;
+        unordered_map<int, int> child, parent;
+        
+        int n = descriptions.size();
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(mp.find(descriptions[i][0]) == mp.end())
+            {
+                TreeNode* node = new TreeNode(descriptions[i][0]);
+                mp[descriptions[i][0]] = node;
+            }
+            if(mp.find(descriptions[i][1]) == mp.end())
+            {
+                TreeNode* node = new TreeNode(descriptions[i][1]);
+                mp[descriptions[i][1]] = node;
+            }
+            if(descriptions[i][2] == 1)
+            {
+                mp[descriptions[i][0]]->left = mp[descriptions[i][1]];
+            }
+            else
+            {
+                mp[descriptions[i][0]]->right = mp[descriptions[i][1]];
+            }
+            parent[descriptions[i][0]]++;
+            child[descriptions[i][1]]++;
+        }
+        
+        TreeNode* ans;
+        for(auto it : mp)
+        {
+            if(parent.find(it.first) != parent.end() && child.find(it.first) == child.end())
+            {
+                ans = it.second;
+                break;
+            }
+        }
+        return ans;
+    }
+};
