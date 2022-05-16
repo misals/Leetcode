03b12bb9cdad035352f899ada005859bbed5a6c3
Suraj Misal <35091245+misals@users.noreply.github.com>
2022-05-16 17:15:35 +0530

    Added solution - LeetHub

---
+++ Non Repeating Numbers - GFG/non-repeating-numbers.cpp
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> singleNumber(vector<int> nums) 
+    {
+        // Code here.
+        int n = nums.size();
+        int XOR = nums[0];
+        for(int i = 1; i < n ; i++) {
+            XOR = XOR ^ nums[i];
+        }
+        int right_bit = XOR & ~(XOR - 1);
+        int x = 0;
+        int y = 0;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] & right_bit) {
+                x = x ^ nums[i];
+            } else {
+                y = y ^ nums[i];
+            }
+        }
+        vector<int> ans;
+        ans.push_back(x);
+        ans.push_back(y);
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	vector<int> v(2 * n + 2);
+    	for(int i = 0; i < 2 * n + 2; i++)
+    		cin >> v[i];
+    	Solution ob;
+    	vector<int > ans = ob.singleNumber(v);
+    	for(auto i: ans)
+    		cout << i << " ";
+    	cout << "\n";
+    }
+	return 0;
+}  // } Driver Code Ends
