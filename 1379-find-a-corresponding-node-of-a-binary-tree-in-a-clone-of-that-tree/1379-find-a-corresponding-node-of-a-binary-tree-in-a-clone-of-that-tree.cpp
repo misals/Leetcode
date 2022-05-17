a31e32ecbd1bacf052585df8871912ccb9944bac
Suraj Misal <35091245+misals@users.noreply.github.com>
2022-05-17 10:53:33 +0530

    Time: 545 ms (88.86%), Space: 163.9 MB (58.30%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -11,42 +11,24 @@
 class Solution {
 public:
     TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
-        if(original -> val == target -> val) {
+        return search(original, cloned, target);
+    }
+    
+    TreeNode* search(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+        if(cloned == NULL) {
+            return NULL;
+        }
+        if(original == target) {
             return cloned;
         }
-        
-        queue<TreeNode*> q1, q2;
-        q1.push(original);
-        q2.push(cloned);
-        
-        TreeNode* ans = NULL;
-        bool flag = false;
-        
-        while(!q1.empty()) {
-            int n = q1.size();
-            for(int i = 0; i < n; i++)
-            {
-                auto p = q1.front();
-                auto q = q2.front();
-                q1.pop();
-                q2.pop();
-                if(p -> val == target -> val) {
-                    ans = q;
-                    flag = true;
-                    break;
-                }
-                if(p -> left) {
-                    q1.push(p -> left);
-                    q2.push(q -> left);
-                }
-                if(p -> right) {
-                    q1.push(p -> right);
-                    q2.push(q -> right);
-                }
-            }
-            if(flag)
-                break;
+        TreeNode* left = search(original -> left, cloned -> left, target);
+        if(left) {
+            return left;
         }
-        return ans;
+        TreeNode* right = search(original -> right, cloned -> right, target);
+        if(right) {
+            return right;
+        }
+        return NULL;
     }
 };
