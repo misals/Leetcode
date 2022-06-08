fe6dc26a8fa26c40f5120a3f8647aec8f15991b0
Suraj Misal <35091245+misals@users.noreply.github.com>
2022-06-08 15:56:17 +0530

    Time: 2 ms (46.92%), Space: 6 MB (73.66%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,24 +1,13 @@
 class Solution {
-public:
-    int dp[101][101];
-    int findAll(int i, int j, int n, int m) {
-        if(i < 0 || i >= n || j < 0 || j >= m) {
-            return 0;
-        }
-        if(i == n - 1 && j == m - 1) {
-            return 1;
-        }
-        if(dp[i][j] != 0) {
-            return dp[i][j];
-        }
-        
-        int bottom = findAll(i+1, j, n, m);
-        int right = findAll(i, j+1, n, m);
-        
-        return dp[i][j] = bottom + right;
-    }
-    
+public:   
     int uniquePaths(int m, int n) {
-        return findAll(0, 0, n, m);
+        int N = m + n - 2;
+        int r = m - 1;
+        double res = 1;
+        
+        for(int i = 1; i <= r; i++) {
+            res = res * (N - r + i) / i;
+        }
+        return (int)res;
     }
 };
