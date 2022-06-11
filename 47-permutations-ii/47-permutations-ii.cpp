7b33bae3c744e4a45ef5d9847799e694fd05ecf8
Suraj Misal <35091245+misals@users.noreply.github.com>
2022-06-11 16:56:44 +0530

    Time: 4 ms (91.10%), Space: 9.1 MB (61.05%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    void helper(int ind, vector<int> &nums, vector<vector<int>> &res, vector<int> &v, int freq[]) {
-        if(v.size() == nums.size()) {
-            res.push_back(v);
+    void generateAll(int ind, vector<int> nums, vector<vector<int>> &ans) {
+        if(ind == nums.size() - 1) {
+            ans.push_back(nums);
             return;
         }
-        for(int i = 0; i < nums.size(); i++) {
-            if(i > 0 && nums[i-1] == nums[i] && freq[i-1]) {
+        for(int i = ind; i < nums.size(); i++) {
+            if(i != ind && nums[i] == nums[ind]) {
                 continue;
-            }
-            if(freq[i]) {
-                v.push_back(nums[i]);
-                freq[i] = 0;
-                helper(ind + 1, nums, res, v, freq);
-                v.pop_back();
-                freq[i] = 1;
-            }
+            } 
+            swap(nums[ind], nums[i]);
+            generateAll(ind + 1, nums, ans);
         }
     }
     
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int>> res;
         sort(nums.begin(), nums.end());
-        int freq[nums.size()];
-        for(int i = 0; i < nums.size(); i++) freq[i] = 1;
-        vector<int> v;
-        helper(0, nums, res, v, freq);
-        return res;
+        vector<vector<int>> ans;
+        generateAll(0, nums, ans);
+        
+        return ans;
     }
 };
