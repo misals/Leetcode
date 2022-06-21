d39dfa7fb024e1bea848470c43a550cc5521bf08
Suraj Misal <35091245+misals@users.noreply.github.com>
2022-06-21 17:56:22 +0530

    Time: 166 ms (30.45%), Space: 18.5 MB (27.57%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    int f(int ind, int tar, vector<int> &coins, vector<vector<int>> &dp) {
-        if(ind == 0) {
-            if(tar % coins[0] == 0) return tar / coins[0];
-            return 1e9;
-        }
-        if(dp[ind][tar] != -1) return dp[ind][tar];
-        int notPick = f(ind - 1, tar, coins, dp);
-        int pick = 1e9;
-        if(tar >= coins[ind]) {
-            pick = 1 + f(ind, tar - coins[ind], coins, dp);
-        }
-        
-        return dp[ind][tar] = min(pick, notPick);
-    }
-    
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int> (amount+1, -1));
-        int ans = f(n-1, amount, coins, dp);
         
-        if(ans >= 1e9) return -1;
+        vector<int> prev(amount+1, 0), curr(amount+1, 0);
         
-        return ans;
+        for(int i = 1; i <= amount; i++) {
+            if(i % coins[0] == 0) prev[i] = i / coins[0];
+            else prev[i] = 1e9;
+        }
+        
+        for(int ind = 1; ind < n; ind++) {
+            for(int tar = 1; tar <= amount; tar++) {
+                int notPick = prev[tar];
+                int pick = 1e9;
+                if(tar >= coins[ind]) {
+                    pick = 1 + curr[tar - coins[ind]];
+                }
+                curr[tar] = min(pick, notPick);
+            }
+            prev = curr;
+        }
+        if(prev[amount] >= 1e9) return -1;
+        
+        return prev[amount];
     }
 };
