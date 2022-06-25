b3ad37bfee39339b358bd8da9271cf0e23cd2560
Suraj Misal <35091245+misals@users.noreply.github.com>
2022-06-25 14:46:25 +0530

    Time: 338 ms (5.03%), Space: 402.5 MB (5.17%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
+    int fun(int i, int j, string s, string t, vector<vector<int>> &dp) {
+        if(i < 0) return j + 1;
+        if(j < 0) return i + 1;
         
-        vector<vector<int>> dp(n + 1, vector<int> (m + 1, 0));
-        for(int i = 0; i <= n; i++) {
-            dp[i][0] = i;
-        }
-        for(int i = 0; i <= m; i++) {
-            dp[0][i] = i;
-        }
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= m; j++) {
-                if(word1[i - 1] == word2[j - 1]) {
-                    dp[i][j] = dp[i-1][j-1];
-                } else {
-                    dp[i][j] = min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j-1])) + 1;
-                }
-            }
-        }
-        return dp[n][m];
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        if(s[i] == t[j]) return fun(i-1, j-1, s, t, dp);
+        
+        return dp[i][j] =  1 + min(fun(i-1, j, s, t, dp), min(fun(i, j-1, s, t, dp), fun(i-1, j-1, s, t, dp)));
+    }
+    
+    int minDistance(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        
+        vector<vector<int>> dp(n, vector<int> (m,-1));
+        return fun(n-1, m-1, s, t, dp);
     }
 };
