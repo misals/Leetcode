3f405814d638e8bf5ea42d4efc05ee6baba06293
Suraj Misal <35091245+misals@users.noreply.github.com>
2022-06-25 09:54:35 +0530

    Time: 19 ms (99.53%), Space: 26.9 MB (81.98%) - LeetHub

---
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int cnt = 0;
+        int n = nums.size();
+        int maxi = nums[0];
+        
+        for(int i = 0; i < n - 1; i++) {
+            if(nums[i] > nums[i+1]) {
+                if(i + 2 < n) {
+                    if(nums[i] < nums[i+2]) {
+                        nums[i+1] = nums[i];
+                    }
+                    else if(nums[i] > nums[i+1]) {
+                        nums[i] = nums[i+1];
+                    }
+                    else {
+                        nums[i+1] = nums[i];
+                    }
+                }
+                else {
+                    nums[i+1] = nums[i];
+                }
+                break;
+            }
+        }
+        for(int i = 0; i < n - 1; i++) {
+            if(nums[i] > nums[i+1])
+                return false;
+        }
+        return true;
+    }
+};
