91e86a674d7aa1c8bf686fd349bda35f942926c3
Suraj Misal <35091245+misals@users.noreply.github.com>
2022-06-25 13:26:37 +0530

    Time: 48 ms (29.69%), Space: 27.1 MB (53.59%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -9,7 +9,7 @@ public:
                 if(cnt == 1) 
                     return false;
                 cnt++;
-                if(i < 2 || nums[i-2] <= nums[i]) {
+                if(i == 1 || nums[i-2] <= nums[i]) {
                     nums[i-1] = nums[i];
                 }
                 else {
