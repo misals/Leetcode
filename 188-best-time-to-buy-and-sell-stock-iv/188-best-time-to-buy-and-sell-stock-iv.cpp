3fa3257afcc5a4e061eda67f9b0d0fa0518d2c43
Suraj Misal <35091245+misals@users.noreply.github.com>
2022-07-01 09:41:16 +0530

    Time: 0 ms (100.00%), Space: 10.8 MB (93.30%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -4,22 +4,24 @@ public:
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
         
-        vector<vector<int>> dp(n+1, vector<int> (2*k+1, 0));
+        vector<int> after(2*k+1, 0);
+        vector<int> curr(2*k+1, 0);
         
         for(int ind = n - 1; ind >= 0; ind--) {
             for(int tran = 0; tran < 2*k; tran++) {
                 if(tran % 2 == 0) {
-                    dp[ind][tran] = max(-prices[ind] + dp[ind+1][tran+1],
-                              0 + dp[ind+1][tran]);
+                    curr[tran] = max(-prices[ind] + after[tran+1],
+                              0 + after[tran]);
                 }
                 else {
-                    dp[ind][tran] = max(prices[ind] + dp[ind+1][tran+1],
-                            0 + dp[ind+1][tran]);
+                    curr[tran] = max(prices[ind] + after[tran+1],
+                            0 + after[tran]);
                 }
             }
+            after = curr;
         }
         
-        return dp[0][0];
+        return after[0];
         
     }
 };
