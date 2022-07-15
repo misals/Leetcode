3e5d40e4863aa70ff1a7df2285e483bd9e36e6ca
Suraj Misal <35091245+misals@users.noreply.github.com>
2022-07-15 08:54:37 +0530

    Time: 24 ms (74.49%), Space: 24 MB (29.56%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,34 +1,44 @@
 class Solution {
 public:
-    void maxAreaOfIsland(int r, int c, vector<vector<int>>& grid, vector<vector<int>> &vis, int &count, int n, int m) {
-        if(r < 0 || r >= n || c < 0 || c >= m || vis[r][c] || grid[r][c] == 0) {
-            return;
+    int di[4] = {0, -1, 0, 1};
+    int dj[4] = {-1, 0, 1, 0};
+    
+    bool isValid(int i, int j, int n, int m) {
+        if(i < 0 || i >= n || j < 0 || j >= m) {
+            return false;
         }
-        vis[r][c] = 1;
-        count++;
-        maxAreaOfIsland(r, c - 1, grid, vis, count, n, m);
-        maxAreaOfIsland(r - 1, c, grid, vis, count, n, m);
-        maxAreaOfIsland(r, c + 1, grid, vis, count, n, m);
-        maxAreaOfIsland(r + 1, c, grid, vis, count, n, m);
+        return true;
+    }
+    
+    
+    void dfs(int x, int y, int n, int m, int &cnt, vector<vector<int>>& grid, vector<vector<int>>& vis) {
+        if(isValid(x, y, n, m) && grid[x][y] == 1 && vis[x][y] == 0) {
+            cnt++;
+            vis[x][y] = 1;
+            for(int i = 0; i < 4; i++) {
+                int a = x + di[i];
+                int b = y + dj[i];
+                dfs(a, b, n, m, cnt, grid, vis);
+            }
+        }
     }
     
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         
-        int res = 0, count = 0;
+        vector<vector<int>> vis(n, vector<int> (m, 0));
+        int ans = 0;
         
-        vector<vector<int>> vis(n, vector<int> (m));
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++)
-            {
-                if(vis[i][j] == 0) {
-                    count = 0;
-                    maxAreaOfIsland(i, j, grid, vis, count, n, m);
-                    res = max(res, count);
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 1 && vis[i][j] == 0) {
+                    int cnt = 0;
+                    dfs(i, j, n, m, cnt, grid, vis);
+                    ans = max(ans, cnt);
                 }
             }
         }
-        return res;
+        return ans;
     }
 };
