f78edd0524062291fe8d81a978487a96c16c798c
Suraj Misal <35091245+misals@users.noreply.github.com>
2022-07-16 12:03:19 +0530

    Time: 491 ms (44.99%), Space: 6.5 MB (80.51%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -1,18 +1,5 @@
 class Solution {
 public:
-    int findCuts(int i, int n, string &s, vector<int> &dp) {
-        if(i == n) return 0;
-        
-        if(dp[i] != -1) return dp[i];
-        int minCost = INT_MAX;
-        for(int j = i; j < n; j++) {
-            if(isPalindrome(i, j, s)) {
-                int cost = 1 + findCuts(j + 1, n, s, dp);
-                minCost = min(minCost, cost);
-            }
-        }
-        return dp[i] = minCost;
-    }
     
     bool isPalindrome(int start, int end, string &s) {
         while(start < end) {
@@ -25,7 +12,19 @@ public:
     
     int minCut(string s) {
         int n = s.size();
-        vector<int> dp(n+1, -1);
-        return findCuts(0, n, s, dp) - 1;
+        vector<int> dp(n+1, 0);
+        
+        for(int i = n - 1; i >= 0; i--) {
+            int minCost = INT_MAX;
+            for(int j = i; j < n; j++) {
+                if(isPalindrome(i, j, s)) {
+                    int cost = 1 + dp[j+1];
+                    minCost = min(minCost, cost);
+                }
+            }
+            dp[i] = minCost;
+        }
+        
+        return dp[0] - 1;
     }
 };
