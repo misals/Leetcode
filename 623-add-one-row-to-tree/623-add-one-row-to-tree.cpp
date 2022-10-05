3d22a90e5b85db0c98d888b99888d4b7b8b47a05
Suraj Misal <35091245+misals@users.noreply.github.com>
2022-10-05 17:44:03 +0530

    Time: 46 ms (13.75%), Space: 25.1 MB (36.94%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.cpp
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.cpp
@@ -11,35 +11,45 @@
  */
 class Solution {
 public:
-    TreeNode* preorder(TreeNode* root, int val, int depth) {
-        if(root == NULL) {
-            return NULL;
-        }
-        
-        if(depth == 2) {
-            TreeNode* leftNode = new TreeNode(val);
-            TreeNode* rightNode = new TreeNode(val);
-            
-            leftNode -> left = root -> left;
-            rightNode -> right = root -> right;
-            
-            root -> left = leftNode;
-            root -> right = rightNode;
-            
-            return root;
-        }
-        root -> left = preorder(root -> left, val, depth - 1);
-        root -> right = preorder(root -> right, val, depth - 1);
-        
-        return root;
-    }
-    
     TreeNode* addOneRow(TreeNode* root, int val, int depth) {
         if(depth == 1) {
             TreeNode* newNode = new TreeNode(val);
             newNode -> left = root;
             return newNode;
         }
-        return preorder(root, val, depth);
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(depth > 2) {
+            int n = q.size();
+            for(int i = 0; i < n; i++) {
+                auto node = q.front();
+                q.pop();
+                if(node -> left != NULL) {
+                    q.push(node -> left);
+                }
+                if(node -> right != NULL) {
+                    q.push(node -> right);
+                }
+            }
+            depth--;
+        }
+        
+        int n = q.size();
+        for(int i = 0; i < n; i++) {
+            auto node = q.front();
+            q.pop();
+            
+            TreeNode* leftNode = new TreeNode(val);
+            TreeNode* rightNode = new TreeNode(val);
+            
+            leftNode -> left = node -> left;
+            rightNode -> right = node -> right;
+            
+            node -> left = leftNode;
+            node -> right = rightNode;
+        }
+        return root;
+        
     }
 };
