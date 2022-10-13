10bb390c26419acc9451c3782e743e82a91ac547
Suraj Misal <35091245+misals@users.noreply.github.com>
2022-10-13 09:42:40 +0530

    Time: 27 ms (44.26%), Space: 7.7 MB (37.95%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -8,22 +8,11 @@
  */
 class Solution {
 public:
-    vector<int> v;
-    void traverse(ListNode* node) {
-        while(node != NULL) {
-            v.push_back(node -> val);
-            node = node -> next;
-        }
-    }
-    
     void deleteNode(ListNode* node) {
-        traverse(node);
-        int n = v.size();
-        
         ListNode* prev = node;
         
-        for(int i = 1; i < n; i++) {
-            node -> val = v[i];
+        while(node -> next != NULL) {
+            node -> val = node -> next -> val;
             prev = node;
             node = node -> next;
         }
