cef7767b52e0aefdc900c61181565f92cc24d847
Suraj Misal <35091245+misals@users.noreply.github.com>
2022-12-02 08:53:57 +0530

    Time: 280 ms (35.93%), Space: 20.5 MB (64.37%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -5,23 +5,18 @@ public:
         int m = t.size();
         
         if(n != m) return false;
-        
-        vector<int> ch1(26, 0);
-        vector<int> ch2(26, 0);
-        
+      
         vector<int> v1(26, 0);
         vector<int> v2(26, 0);
         
         for(int i = 0; i < n; i++) {
-            ch1[s[i] - 'a'] = 1;
-            ch2[t[i] - 'a'] = 1;
             
             v1[s[i] - 'a']++;
             v2[t[i] - 'a']++;
         }
         
         for(int i = 0; i < 26; i++) {
-            if(ch1[i] != ch2[i]) {
+            if((v1[i] > 0 && v2[i] == 0) || (v1[i] == 0 && v2[i] > 0)) {
                 return false;
             }
         }
