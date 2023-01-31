2f8337973564b5d8369f288eeb1f990be829fd73
Suraj Misal <35091245+misals@users.noreply.github.com>
2023-01-31 10:31:28 +0530

    Time: 186 ms (76.95%), Space: 20.1 MB (50.97%) - LeetHub

---
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int bestTeamScore(vector<int>& scores, vector<int>& ages) {
+        vector<pair<int, int>> v;
+        int n = scores.size();
+        
+        for(int i = 0; i < n; i++) {
+            v.push_back({ages[i], scores[i]});
+        }
+        sort(v.begin(), v.end());
+        vector<int> dp(n);
+        
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            dp[i] = v[i].second;
+            
+            for(int j = 0; j < i; j++) {
+                if(v[j].second <= v[i].second) {
+                    dp[i] = max(dp[i], dp[j] + v[i].second);
+                }
+            }
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+    }
+};
