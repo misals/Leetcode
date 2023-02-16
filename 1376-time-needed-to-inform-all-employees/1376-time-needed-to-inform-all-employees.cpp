a0ae1d141f726cd284581d31b9818f5331347322
Suraj Misal <35091245+misals@users.noreply.github.com>
2023-02-16 10:02:34 +0530

    Time: 371 ms (48.97%), Space: 112.8 MB (83.29%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> adj[100005];
+    
+    int dfs(int headID, vector<int> &informTime) {
+        int maxi = 0;
+        for(auto it : adj[headID]) {
+            maxi = max(maxi, dfs(it, informTime));
+        }
+        return informTime[headID] + maxi;
+    }
+    
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        
+        for(int i = 0; i < n; i++) {
+            if(manager[i] != -1) {
+                adj[manager[i]].push_back(i);
+            }
+        }
+        
+        return dfs(headID, informTime);
+    }
+};
