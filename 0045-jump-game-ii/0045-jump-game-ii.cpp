4fc5528e94e591cee9674de398923b09e90d2416
Suraj Misal <35091245+misals@users.noreply.github.com>
2023-02-20 15:25:04 +0530

    Time: 12 ms (89.29%), Space: 16.6 MB (83.45%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        int jump = 0;
+        int cnt = 0;
+        int maxi = 0;
         int curr = 0;
-        int far = 0;
         
         for(int i = 0; i < n - 1; i++) {
-            far = max(far, i + nums[i]);
+            maxi = max(maxi, i + nums[i]);
             if(i == curr) {
-                jump++;
-                curr = far;
-            }
+                cnt++;
+                curr = maxi;
+            }  
         }
-        return jump;
-        
+        return cnt;
     }
 };
