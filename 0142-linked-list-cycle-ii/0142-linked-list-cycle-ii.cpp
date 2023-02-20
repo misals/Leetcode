fab6fc0af33ab697e61c4d352c8aadf26bd54034
Suraj Misal <35091245+misals@users.noreply.github.com>
2023-02-20 22:19:28 +0530

    Time: 23 ms (10.12%), Space: 10 MB (9.97%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -12,31 +12,15 @@ public:
         if(head == NULL || head -> next == NULL) {
             return NULL;
         }
-        ListNode* slow = head;
-        ListNode* fast = head;
-        bool flag = false;
-        
-        while(fast != NULL && fast -> next != NULL) {
-            
-            slow = slow -> next;
-            fast = fast -> next -> next;
-            
-            if(slow == fast) {
-                flag = true;
-                break;
+        int i = 0;
+        map<ListNode*, int> mp;
+        while(head != NULL) {
+            if(mp.find(head) != mp.end()) {
+                return head;
             }
-        }
-        if(flag) {
-            int i = 0;
-            map<ListNode*, int> mp;
-            while(head != NULL) {
-                if(mp.find(head) != mp.end()) {
-                    return head;
-                }
-                mp[head] = i;
-                head = head -> next;
-                i++;
-            }
+            mp[head] = i;
+            head = head -> next;
+            i++;
         }
         return NULL;
         
