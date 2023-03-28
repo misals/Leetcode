e9e661b2e9cf137c18ef516dbba95e12ac7f86d0
Suraj Misal <35091245+misals@users.noreply.github.com>
2023-03-28 09:42:07 +0530

    Time: 145 ms (29.45%), Space: 78 MB (5.00%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
-    int solve(int ind, int amount, vector<int> &coins, vector<vector<long long>> &dp) {
-        if(ind == 0) {
-            if(amount % coins[ind] == 0) {
-                return amount / coins[ind];
-            }
-            return 1e5;
-        }
-        if(dp[ind][amount] != -1) return dp[ind][amount];
-        
-        long long notPick = solve(ind - 1, amount, coins, dp);
-        long long pick = 1e5;
-        
-        if(amount >= coins[ind]) {
-            pick = 1 + solve(ind, amount - coins[ind], coins, dp);
-        }
-        
-        return dp[ind][amount] = min(pick, notPick);
-    }
     
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         
         vector<vector<long long>> dp(n, vector<long long> (amount + 1, -1));
         
-        long long ans = solve(n - 1, amount, coins, dp);
+        for(int i = 0; i <= amount; i++) {
+            if(i % coins[0] == 0) {
+                dp[0][i] = i / coins[0];
+            }
+            else {
+                dp[0][i] = 1e5;
+            }
+        }
+        
+        for(int ind = 1; ind < n; ind++) {
+            for(int tar = 0; tar <= amount; tar++) {
+                long long notPick = dp[ind - 1][tar];
+                long long pick = 1e5;
+
+                if(tar >= coins[ind]) {
+                    pick = 1 + dp[ind][tar - coins[ind]];
+                }
+
+                dp[ind][tar] = min(pick, notPick);
+            }
+        }
+        
+        long long ans = dp[n - 1][amount];
         
         if(ans == 1e5) {
             return -1;
