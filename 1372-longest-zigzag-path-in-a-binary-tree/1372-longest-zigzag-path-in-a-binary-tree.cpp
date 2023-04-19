3433c645ead7f759718d00e8090b601447e1a688
Suraj Misal <35091245+misals@users.noreply.github.com>
2023-04-19 10:09:34 +0530

    Time: 173 ms (65.16%), Space: 99.5 MB (42.33%) - LeetHub

---
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int longestZigZag(TreeNode* root) {
+        queue<pair<TreeNode*, pair<int, int>>> q;
+        
+        q.push({root, {0, -1}});
+        
+        int ans = 0;
+        int level = 0;
+        
+        while(!q.empty()) {
+            int n = q.size();
+            for(int i = 0; i < n; i++) {
+                auto node = q.front();
+                q.pop();
+                ans = max(ans, node.second.first);
+                
+                if(node.first -> left != NULL) {
+                    if(node.second.second != 0) {
+                        q.push({node.first -> left, {node.second.first + 1, 0}});
+                    }
+                    else {
+                        q.push({node.first -> left, {1, 0}});
+                    }   
+                }
+                
+                if(node.first -> right != NULL) {
+                    if(node.second.second != 1) {
+                        q.push({node.first -> right, {node.second.first + 1, 1}});
+                    }
+                    else {
+                        q.push({node.first -> right, {1, 1}});
+                    }   
+                }
+            }
+            
+            //cout << level << " " << ans << endl;
+           // level++;
+        }
+        return ans;
+    }
+};
