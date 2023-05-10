98d44af191e802ff5c355a794413a2af0a2bb0ad
Suraj Misal <35091245+misals@users.noreply.github.com>
2023-05-10 09:19:42 +0530

    Time: 667 ms (14.23%), Space: 168 MB (28.35%) - LeetHub

--- 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.cpp
+++ 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
-        int i,j,m=mat.size(),n=mat[0].size();
-        vector<vector<int>> v(m*n);
-        vector<int> r(m,0),c(n,0);
-        for(i = 0; i < m; i++){
-            for(j = 0; j < n; j++){
-                v[mat[i][j]-1] = {i,j};
+        int n = mat.size();
+        int m = mat[0].size();
+        
+        map<int, pair<int, int>> mp;
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                mp[mat[i][j]] = {i, j};
             }
         }
-        for(i = 0; i < m*n; i++){
-            r[v[arr[i]-1][0]]++;
-            c[v[arr[i]-1][1]]++;
-            if(r[v[arr[i]-1][0]] == n)return i;
-            if(c[v[arr[i]-1][1]] == m)return i;
+        
+        map<int, int> row, col;
+        
+        int sz = arr.size();
+        
+        for(int ind = 0; ind < sz; ind++) {
+            int i = mp[arr[ind]].first;
+            int j = mp[arr[ind]].second;
+            
+            row[i]++;
+            col[j]++;
+            
+            if(row[i] == m || col[j] == n) {
+                return ind;
+            }
         }
+        
         return -1;
     }
 };
