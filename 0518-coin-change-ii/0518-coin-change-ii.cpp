af1c057806c279f9837896d2ad5185614bc9a214
Suraj Misal <35091245+misals@users.noreply.github.com>
2023-08-11 10:30:08 +0530

    Time: 24 ms (59.25%), Space: 7.3 MB (86.45%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<int> prev(amount+1, 0), curr(amount+1, 0);
+        for(int i = 0; i <= amount; i++) {
+            if(i % coins[0] == 0)
+                prev[i] = 1;
+        }
+        
+        for(int ind = 1; ind < n; ind++) {
+            for(int tar = 0; tar <= amount; tar++) {
+                int notPick = prev[tar];
+                int pick = 0;
+                if(tar >= coins[ind]) {
+                    pick = curr[tar - coins[ind]];
+                }
+                curr[tar] = pick + notPick;
+            }
+            prev = curr;
+        }
+        return prev[amount];
+    }
+};
